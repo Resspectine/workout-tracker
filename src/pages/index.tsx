912f5340ff723db84edfd32d29a1e1b2ce1ef2bb
Maksim Stefanenko <resspectyn@gmail.com>
2023-02-18 17:09:12 +0300

    Add configuration page, add page with trainings days displayed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,34 @@
+import type { GetServerSideProps } from 'next';
 import { type NextPage } from 'next';
+import { useState } from 'react';
+import { Tracker } from '../components/Tracker';
+import type { Configuration } from '../components/TrackerConfiguration';
+import { CONFIGURATION_KEY, TrackerConfiguration } from '../components/TrackerConfiguration';
 
-const Home: NextPage = () => {
+export const getServerSideProps: GetServerSideProps = ({ req }) => {
+  return Promise.resolve({
+    props: {
+      savedConfiguration: JSON.parse(req.cookies[CONFIGURATION_KEY] || 'null') as Configuration,
+    },
+  });
+};
+
+const Home: NextPage<{ savedConfiguration: Configuration | null }> = ({ savedConfiguration }) => {
+  const [isConfigured, setIsConfigured] = useState(!!savedConfiguration);
+
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">Hello World!</h1>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-[#15162c]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <h1 className="text-5xl font-extrabold text-white sm:text-[4rem]">Track yor paid workouts</h1>
+          {isConfigured ? (
+            <Tracker savedConfiguration={savedConfiguration} />
+          ) : (
+            <TrackerConfiguration
+              savedConfiguration={savedConfiguration}
+              onConfigurationSave={() => setIsConfigured(true)}
+            />
+          )}
         </div>
       </main>
     </>
